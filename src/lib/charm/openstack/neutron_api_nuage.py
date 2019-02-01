2a47518864c859957f820f132529d9f8b60ec51e
sunnyve <sunny.verma@alcatel-lucent.com>
2019-02-01 16:59:50 -0500

    initial commit

---
+++ src/lib/charm/openstack/neutron_api_nuage.py
@@ -0,0 +1,64 @@
+import socket
+import subprocess
+
+import charmhelpers.core.hookenv as hookenv
+import charmhelpers.contrib.network.ip as ch_ip
+import charms_openstack.charm
+import charms_openstack.adapters
+from charmhelpers.fetch import (
+    apt_install,
+    add_source,
+)
+
+ML2_CONF = '/etc/neutron/plugins/ml2_conf.ini'
+VXLAN = 'vxlan'
+NUAGE_PACKAGES = ['nuage-openstack-neutron', 'nuage-openstack-neutronclient']
+
+class NeutronApiNuageCharm(charms_openstack.charm.OpenStackCharm):
+
+    # Internal name of charm
+    service_name = name = 'neutron-api-nuage'
+
+    # First release supported
+    release = 'Queens'
+
+    # List of packages to install for this charm
+    packages = NUAGE_PACKAGES
+
+    required_relations = ['neutron-plugin-api-subordinate']
+
+    service_plugins = 'NuagePortAttributes,NuageAPI,NuageL3'
+
+    def configure_plugin(self, api_principle):
+        """Add sections and tuples to insert values into neutron-server's
+        neutron.conf
+        """
+        inject_config = {
+            "neutron-api": {
+                "/etc/neutron/neutron.conf": {
+                    "sections": {
+                        'DEFAULT': [
+                        ],
+                    }
+                }
+            }
+        }
+
+        api_principle.configure_plugin(
+            neutron_plugin='vsp',
+            core_plugin='neutron.plugins.ml2.plugin.Ml2Plugin',
+            neutron_plugin_config='/etc/neutron/plugins/ml2/ml2_conf.ini',
+            service_plugins=self.service_plugins,
+            subordinate_configuration=inject_config)
+
+    def install(self):
+        '''
+        Install hook is run when the charm is first deployed on a node.
+        '''
+        configure_sources(config('nuage-extra-source'), config('nuage-extra-key'))
+        status_set('maintenance', 'Installing apt packages')
+        apt_update()
+        pkgs =NUAGE_PACKAGES
+        for pkg in pkgs:
+            apt_install(pkg, options=['--force-yes', '--allow-unauthenticated'], fatal=True)
+
